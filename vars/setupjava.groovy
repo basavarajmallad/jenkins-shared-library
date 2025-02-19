1031d9354f45de8f68967b0d9812b351f9986e7e
basavarajmallad <basavarajmallad@outlook.com>
2025-02-19 20:18:49 +0530

    Update setupjava.groovy

--- vars/setupjava.groovy
+++ vars/setupjava.groovy
@@ -1,5 +1,7 @@
-def call() {
+def call(cmd) {
     echo " installing java 17"
     sh "sudo apt update"
-    sh "sudo apt install -y openjdk-17-jdk"
+   // sh "sudo apt install -y openjdk-17-jdk"
+    echo " ${cmd}"
+      sh "sudo apt install -y ${cmd}"
 }
